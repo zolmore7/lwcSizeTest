bbf67531ec8c8768a18740d86cb99e1f9990680a
Danny <zolmore@gmail.com>
2021-06-24 17:52:15 -0400

    Adding allow origin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.end(JSON.stringify({ a: "Hello World" }));
 })
 
