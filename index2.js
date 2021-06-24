5d0f754d76b6c396c42da19d76fe8d9df382db8c
Danny <zolmore@gmail.com>
2021-06-24 17:44:31 -0400

    Fixed port issue

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const fs = require('fs');
+var port = process.env.PORT || 3000;
 
 const options = {
   key: fs.readFileSync('key.pem'),
@@ -9,4 +10,4 @@ const options = {
 https.createServer(options, function (req, res) {
   res.writeHead(200);
   res.end("hello world\n");
-}).listen(5123);
+}).listen(port);
