22c34a317b6603592c943efcc29b72009e9f1eba
Danny <zolmore@gmail.com>
2021-06-24 17:47:39 -0400

    Changed port of not https version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5123
+var port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send({"data" : "Hello World!"})
