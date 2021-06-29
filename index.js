7dc75416ddfa48c862746291a4168e0815fbdcd5
Danny <zolmore@gmail.com>
2021-06-29 13:31:05 -0400

    Adding async fetch

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ returnVar += 'DannyLeblanc12345';
 app.get('/', (req, res) => {
   let dataReturn = '';
   console.log('Before fetch');
-  fetch('https://file-examples-com.github.io/uploads/2017/10/file_example_TIFF_10MB.tiff')
-  .then(res => res.text())
-  .then(data => {
-    dataReturn = data;
-    console.log(data);
-  })
+  doFetch();
   console.log('After fetch');
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   //res.end(returnVar);
   res.end(dataReturn);
 })
+
+async function doFetch() {
+  const data = await fetch('https://file-examples-com.github.io/uploads/2017/10/file_example_TIFF_10MB.tiff');
+  return data;
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
